94d340b4c7fb0955a4d681d6295570a3036ebcb4
atxjy <3360907452@qq.com>
2022-10-23 17:22:36 +0800

    master first commit

---
+++ src/main/java/com/atxjy/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atxjy.git;
+
+/**
+ * @author xjy
+ * @create 2022-10-23 17:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
