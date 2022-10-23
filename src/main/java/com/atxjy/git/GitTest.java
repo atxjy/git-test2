8213245762b522719fa73c246f297b8508615761
atxjy <3360907452@qq.com>
2022-10-23 17:24:45 +0800

    master second commit

--- src/main/java/com/atxjy/git/GitTest.java
+++ src/main/java/com/atxjy/git/GitTest.java
@@ -7,5 +7,6 @@ package com.atxjy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
